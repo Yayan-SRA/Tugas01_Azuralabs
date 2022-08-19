8af1d23ddfe8d1c27af60567e05daddcbcbd1b08
Yayan-SRA <yayansra14@gmail.com>
2022-08-19 14:47:22 +0700

    fix

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const morgan = require('morgan')
-const router = require('../config/routes')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const router = require("../config/routes");
 
+const app = express();
+
+/** Install request logger */
 app.use(morgan("dev"));
-app.use(express.json())
 
-module.exports = router.apply(app)
+/** Install JSON request parser */
+app.use(express.json());
 
+/** Install Router */
+app.use(router);
+
+module.exports = app;
